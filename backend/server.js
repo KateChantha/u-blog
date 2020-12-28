c126d9009cc7b426f789609412fde91be512d923
Kato <kubkate@gmail.com>
2020-12-27 16:25:16 -0800

    connect db

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,20 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 // app
 const app = express();
+
+// db
+mongoose
+    .connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+
+mongoose.connection.on("error", err => {
+        console.log(`DB connection error: ${err.message}`);
+    });
 
 // middlewares
 app.use(morgan('dev'));
