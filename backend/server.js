eb9aa6e23947692eaac85f7725b278cc12b8464f
Kato <kubkate@gmail.com>
2020-12-28 15:35:49 -0800

    setup signup routes and controller

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,10 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
+
 // bring routes
 const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
 
 // app
 const app = express();
@@ -30,8 +32,9 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // routes middleware - with api prefix
-// http://localhost:8000/api
+// http://localhost:8000/api/
 app.use('/api',blogRoutes);
+app.use('/api',authRoutes);
 
 // port
 const port = process.env.PORT || 8000;
