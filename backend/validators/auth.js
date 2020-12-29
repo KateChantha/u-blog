c051b684dfc35717fa509d703ce473a4c9abd474
Kato <kubkate@gmail.com>
2020-12-28 16:06:42 -0800

    signup validator

---
+++ backend/validators/auth.js
@@ -0,0 +1,15 @@
+const { check } = require('express-validator');
+
+// this validation result (error message) will be sent to /validators/index.js
+exports.userSignupValidator = [
+    check('name')
+        .not()
+        .isEmpty()
+        .withMessage('Name is required'),
+    check('email')
+        .isEmail()
+        .withMessage('Must be a valid email address'),
+    check('password')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long')
+];
