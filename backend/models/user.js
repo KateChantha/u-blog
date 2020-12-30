d41c07774ea049a0eed605d2a5822976d4c6254e
Kato <kubkate@gmail.com>
2020-12-29 16:31:27 -0800

    User shcema virtual feild and methods

--- backend/models/user.js
+++ backend/models/user.js
@@ -53,4 +53,44 @@ const userSchema = new mongoose.Schema(
   { timestamp: true }
 );
 
+userSchema
+    .virtual('password')
+    .set(function(password) {
+        // create a temporarity variable called _password
+        this._password = password;
+        // generate salt
+        this.salt = this.makeSalt();
+        // encryptPassword
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    });
+
+userSchema.methods = {
+    // method will be used during user sign in
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
+    // method to hash password
+    encryptPassword: function(password) {
+        if (!password) return '';
+        try {
+            // cyrpto is a module form node - see documentation
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
+    },
+
+    // method to generate salt
+    makeSalt: function() {
+        return Math.round(new Date().valueOf() * Math.random()) + '';
+    }
+};
+
 module.exports = mongoose.model('User', userSchema);
