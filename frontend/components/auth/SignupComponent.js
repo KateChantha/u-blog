ae40987d6e08291d561030f2a701b555edd0ca58
Kato <kubkate@gmail.com>
2021-01-01 15:11:15 -0800

    setup signup form

---
+++ frontend/components/auth/SignupComponent.js
@@ -0,0 +1,55 @@
+import { Fragment } from 'react';
+
+const SignupComponent = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("handle submit")
+  }
+  
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  };
+  
+  const signupForm = () => {
+    return (
+      <form onSubmit={ handleSubmit }>
+        <div className="form-group">
+          <input 
+            onChange={ handleChange }
+            type="text" 
+            className="form-control" 
+            placeholder="Type your name"
+          />
+        </div>
+        <div className="form-group">
+          <input 
+            onChange={ handleChange }
+            type="emailt" 
+            className="form-control" 
+            placeholder="Type your email"
+          />
+        </div>
+        <div className="form-group">
+          <input 
+            onChange={ handleChange }
+            type="password" 
+            className="form-control" 
+            placeholder="Type your password"
+          />
+        </div>
+        <div>
+          <button className="btn btn-primary">Signup</button>
+        </div>
+      </form>
+    )
+  }
+
+  return (
+    <Fragment>
+      { signupForm() }
+    </Fragment>
+    
+  )
+}
+
+export default SignupComponent;
