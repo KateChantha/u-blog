e1ed7820342758c70aeef39b2ca18dce5ce94d9b
Kato <kubkate@gmail.com>
2021-01-01 15:46:52 -0800

    update config

--- frontend/config.js
+++ frontend/config.js
@@ -1,13 +1,13 @@
 import getConfig from 'next/config';
 const { publicRuntimeConfig } = getConfig();
 
-// export const API = publicRuntimeConfig.PRODUCTION
-//     ? publicRuntimeConfig.API_PRODUCTION
-//     : publicRuntimeConfig.API_DEVELOPMENT;
-
 export const API = publicRuntimeConfig.PRODUCTION
-    ? 'https://ublog.com'
-    : 'http://localhost:8000';
+    ? publicRuntimeConfig.API_PRODUCTION
+    : publicRuntimeConfig.API_DEVELOPMENT;
+
+// export const API = publicRuntimeConfig.PRODUCTION
+//     ? 'https://ublog.com'
+//     : 'http://localhost:8000';
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 
