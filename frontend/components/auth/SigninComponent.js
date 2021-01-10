e20a6290ed2aaed28e34fa26bb5f20c9c4858fd9
Kato <kubkate@gmail.com>
2021-01-09 18:27:33 -0800

    set up Signin

---
+++ frontend/components/auth/SigninComponent.js
@@ -0,0 +1,92 @@
+import Router from 'next/router';
+import { useState, Fragment } from 'react';
+import { signin } from '../../actions/auth';
+
+const SigninComponent = () => {
+  const [values, setValues] = useState({
+    email: 'guest@gmail.com',
+    password: 'gggggg',
+    error: '',
+    loading: false,
+    message: '',
+    showForm: true
+  })
+
+  const { email, password, error, loading, message, showForm } = values;
+  /**
+   * step 1. setValues from user form input
+   * step 2. if successful signin, redirect user to home page(for now)
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // console.table({ email, password, error, loading, message, showForm });
+    setValues({ ...values, loading: true, error: false });
+    // read variable name, email, password and store in user
+    const user = { email, password };
+
+    signin(user).then(data => {
+      if (data.error) {
+          setValues({ ...values, error: data.error, loading: false });
+      } else {
+        // save user token to cookie
+        // save user info to local storage
+        // authenticate user
+        Router.push(`/`)
+      }
+    });
+  }
+  
+  const handleChange = type => (e) => {
+    setValues({ 
+      ...values, 
+      // hide an error when user start to fill out form again
+      error: false, 
+      // type of input
+      [type]: e.target.value 
+    });
+  };
+
+  const showLoading = () => (loading? <div className="alert alert-info">Loading...</div> : "");
+  const showError = () => (error? <div className="alert alert-danger">.{error}</div> : "");
+  const showMessage = () => (message? <div className="alert alert-info">{message}</div> : "");
+  
+  const signinForm = () => {
+    return (
+      <form onSubmit={ handleSubmit }>
+        <div className="form-group">
+          <input 
+            value={email}
+            onChange={ handleChange('email') }
+            type="email" 
+            className="form-control" 
+            placeholder="Type your email"
+          />
+        </div>
+        <div className="form-group">
+          <input 
+            value={password}
+            onChange={ handleChange('password') }
+            type="password" 
+            className="form-control" 
+            placeholder="Type your password"
+          />
+        </div>
+        <div>
+          <button className="btn btn-primary">Sign In</button>
+        </div>
+      </form>
+    )
+  }
+
+  return (
+    <Fragment>
+      { showError() }
+      { showLoading() }
+      { showMessage() }
+      { showForm && signinForm() }
+    </Fragment>
+    
+  )
+}
+
+export default SigninComponent;
--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,9 +1,17 @@
 import Layout from '../components/Layout';
+import SigninComponent from '../components/auth/SigninComponent';
 
 const Signin = () => {
   return (
     <Layout>
-      <h2> Signin Page </h2>
+      <div className="container-fluid">
+          <h2 className="text-center pt-4 pb-4">Signin</h2>
+          <div className="row">
+              <div className="col-md-6 offset-md-3">
+                  <SigninComponent />
+              </div>
+          </div>
+      </div>
     </Layout>
   )
 }
