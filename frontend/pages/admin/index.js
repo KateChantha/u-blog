2b8343e2b864c90a558916cf7510e4c3f1b1a696
Kato <kubkate@gmail.com>
2021-01-12 21:07:51 -0800

    initial setup for each admin and user dashboard

---
+++ frontend/pages/admin/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Layout from '../../components/Layout';
+
+
+const AdminIndex = () => {
+  return (
+    <Layout>
+      <h2>Admin Dashboard</h2>
+    </Layout>
+  )
+}
+
+export default AdminIndex;
---
+++ frontend/pages/user/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import Layout from '../../components/Layout';
+
+
+const UserIndex = () => {
+  return (
+    <Layout>
+      <h2>User Dashboard</h2>
+    </Layout>
+  )
+}
+
+export default UserIndex;
