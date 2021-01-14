d6fbe1eb853a8b912029516e94049efb54c2fc34
Kato <kubkate@gmail.com>
2021-01-13 20:20:53 -0800

    create category route

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -8,5 +8,11 @@ const { categoryCreateValidator } = require('../validators/category');
 const { requireSignin, adminMiddleware } = require('../controllers/auth');
 
 router.post('/category', categoryCreateValidator, runValidation, requireSignin, adminMiddleware, create);
+router.get('/categories', list);
+// get a sigle category access by req.params
+// use slug for SEO
+router.get('/category/:slug', read);
+router.delete('/category/:slug', requireSignin, adminMiddleware, remove);
+
 
 module.exports = router;
