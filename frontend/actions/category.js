52881353bc2f846f1be291f422a71b53677fb327
Kato <kubkate@gmail.com>
2021-01-22 18:16:06 -0800

    create functions CRD for category

--- frontend/actions/category.js
+++ frontend/actions/category.js
@@ -14,3 +14,32 @@ export const create = (categoty, token) => {
       .then(response => response.json())
       .catch(err => console.log(err));
 };
+
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .catch(err => console.log(err));
+};
+
+export const singleCategory= (slug) => {
+    return fetch(`${API}/category/${slug}`, {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .catch(err => console.log(err));
+};
+
+export const removeCategory = (slug, token) => {
+    return fetch(`${API}/category/${slug}`, {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+    })
+        .then(response => response.json())
+        .catch(err => console.log(err));
+  };
